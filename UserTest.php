d5960aa0a3681daa492081a93b0fa578649c0d6f
dianag <diana.gamez.fon@gmail.com>
2014-09-22 10:14:44 -0500

    Test success

--- UserTest.php
+++ UserTest.php
@@ -3,7 +3,7 @@ require_once "User.php";
 class UserTest extends PHPUnit_Framework_TestCase{
 	public function testTalk(){
 		$user = new User();
-		$expected = "Hello World!";
+		$expected = "Hello world!";
 		$actual = $user->talk();
 		$this->assertEquals($expected, $actual);
 	}
